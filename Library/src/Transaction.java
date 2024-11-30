8899b3acb42005cbd159baacb1b47b46b143a005
Mark T. Student <mdt030302@gmail.com>
2024-11-30 10:22:07 -0800

    implemnted saveTransaction method

--- Library/src/Transaction.java
+++ Library/src/Transaction.java
@@ -1,4 +1,7 @@
 import java.text.SimpleDateFormat;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
 
 public class Transaction {
@@ -23,6 +26,7 @@ public class Transaction {
             member.borrowBook(book); 
             String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle();
             System.out.println(transactionDetails);
+            saveTransaction(transactionDetails);
             return true;
         } else {
             System.out.println("The book is not available.");
@@ -37,6 +41,7 @@ public class Transaction {
             book.returnBook();
             String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle();
             System.out.println(transactionDetails);
+            saveTransaction(transactionDetails);
         } else {
             System.out.println("This book was not borrowed by the member.");
         }
@@ -46,5 +51,18 @@ public class Transaction {
     private String getCurrentDateTime() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return sdf.format(new Date());
+    }
+    
+    private void saveTransaction(String s) {
+    	try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.txt"));
+			writer.write(s+"\n");
+			writer.close();
+		} catch (IOException e) {
+			
+			System.out.println("IO error: cannot access transactions.txt");
+		}
+    		
+    	
     }
 }
